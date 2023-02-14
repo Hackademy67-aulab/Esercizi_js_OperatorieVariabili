a417ecd5ed373e5cac45865056dfea06e4db0840
Leonardo Colaleo <124148340+leon94H@users.noreply.github.com>
2023-02-14 22:31:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+// let number = 12;
+
+// let compleanno = 2;
+
+// let ciaone = 34;
+
+// let comeOn = 3;
+
+// let vita = 10;
+
+// let somma = number+compleanno+ciaone+comeOn+vita;
+
+
+// let media = somma/2; 
+
+
+
+
+// console.log('La somma fra tutti i numeri è')
+// console.log(somma)
+// console.log('la media tra tutti i numeri è')
+// console.log(media);
+
+
+
+// let number = 2023;
+// let annonuovo= 2023;
+// let datadinascita= 1994;
+
+// console.log(annonuovo-datadinascita)
+// console.log(100-29)
+// console.log('Hai 29 anni e ti mancano 71 anni per arrivare a 100')
+
+
+// let gatti=44;
+// let gattiinognifila=6;
+
+
+// console.log(gatti/gattiinognifila)
+// console.log( 'Ci sono 7 file di gatti e ne mancano 3 per farne una nuova, con il resto di 3')
+
+
+let temp = -6;
+let temp2 = -2;
+let temp3 = 7;
+let temp4 = 10;
+let temp5 = 15;
+let temp6 = 22;
+let temp7 = 31;
+
+console.log(Math.max(-6,-2,7,10,15,22,31));
+console.log(Math.min(-6,-2,7,10,15,22,31));
+
+
+
+
+
+
+
+
+
